fb3a6f9dbbccc4a2647d0e81d3acc3483481baff
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-02-06 16:04:29 +0100

    make sendgrid api key optional

--- tasks/SendMail.js
+++ tasks/SendMail.js
@@ -1,7 +1,29 @@
 const sendgrid = require('@sendgrid/mail');
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+/**
+ * Retrieve the Sendgrid API key from the environment.
+ *
+ * @returns {(string|undefined)}
+ */
+const getSentryApiKey = function() {
+  if (typeof process.env.SENDGRID_API_KEY === "string" && process.env.SENDGRID_API_KEY.length > 0) {
+    return process.env.SENDGRID_API_KEY;
+  }
+
+  return undefined;
+};
 
 module.exports.handle = async function(user) {
+  const sendgridApiKey = getSentryApiKey();
+  // if the sendgrid api key is not provided, we don't send any email.
+  if (!sendgridApiKey) {
+    console.log(`Email notification to ${user.email} has not been sent because no Sendgrid API key was provided.`);
+
+    return false;
+  }
+
+  sendgrid.setApiKey(sendgridApiKey);
+
   // send an email using the Sendgrid SDK.
   await sendgrid.send({
     personalizations: [{ to: [{ email: user.email }] }],
@@ -14,4 +36,6 @@ module.exports.handle = async function(user) {
     subject: "Deal refund",
     from: { email: "zenaton-tutorial@zenaton.com" }
   });
+
+  return true;
 };

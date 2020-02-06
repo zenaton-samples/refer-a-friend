5c38001b7b0afe6ba9c118ae7ee26c62d5459b49
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-02-06 16:05:11 +0100

    add many log statements to ease understanding

--- tasks/Refund.js
+++ tasks/Refund.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 module.exports.handle = async function(user) {
   // this is a fake task doing an HTTP request on a fake endpoint for the example's sake.
   // in a real project, you should make a request to an endpoint in your application or
-  // to your payment gateway.
+  // to your payment gateway API.
   await axios.put("https://httpbin.org/anything/refund", { body: user });
+
+  console.log(`A refund has been granted to user ${user.email}.`);
 };
--- workflows/ReferAFriend.js
+++ workflows/ReferAFriend.js
@@ -2,11 +2,14 @@ module.exports.handle = function*(user) {
   // initialize the counter of friend that have purchased a deal.
   let nb_friends = 0;
 
+  this.log(`Starting workflow ReferAFriend for user ${user.email}.`);
+
   // loop until 3 friends purchased a deal.
   do {
     // wait for the deal_purchased event. every time we receive this event, we increment the counter.
     yield this.wait.event("deal_purchased").forever();
     nb_friends++;
+    this.log(`A friend of user ${user.email} has purchased a deal.`);
   } while (nb_friends < 3);
 
   // trigger the refund to the user in your application.
